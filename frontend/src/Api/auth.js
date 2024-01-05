c8f552548f3d39fd9c717cf8f44c6938536ca692
WatterAH <samueltlahuel.m@gmail.com>
2024-01-04 19:25:12 -0600

    feedgames v1.0

--- frontend/src/Api/auth.js
+++ frontend/src/Api/auth.js
@@ -11,7 +11,7 @@ export const loginApi = async (username, password) => {
     body: JSON.stringify(body),
   });
   const resData = await res.json();
-
+  console.log(document.cookie);
   if (res.ok) {
     return resData;
   } else {
