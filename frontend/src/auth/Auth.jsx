d0d461ea0ce244851a3553f89d381dae686de887
WatterAH <samueltlahuel.m@gmail.com>
2024-01-06 11:40:36 -0600

    feedgames v1.0

--- frontend/src/auth/Auth.jsx
+++ frontend/src/auth/Auth.jsx
@@ -16,18 +16,18 @@ export const Auth = () => {
     }
   };
   return (
-    <div className="flex flex-1 min-h-full flex-col justify-center h-screen px-6 py-12 sm:bg-gray-100">
+    <div className="flex flex-1 min-h-full flex-col justify-center h-screen px-6 py-12 sm:bg-gray-100 relative">
       <div
         className={`flex items-center shadow-transparent justify-center bg-white sm:shadow-xl rounded-xl sm:mx-auto ${
           content == "Login" ? "sm:max-w-sm" : "sm:max-w-md"
-        } sm:w-full relative`}
+        } sm:w-full`}
       >
         {renderContent()}
-        <footer className="absolute -bottom-4 sm:-bottom-10 left-0 w-full flex justify-center items-center gap-x-4">
-          <Link text={"Acerca de Feedgames"} content={"About"} />
-          <Link text={"Aviso para los usuarios de iOS"} content={"Issues"} />
-        </footer>
       </div>
+      <footer className="absolute bottom-5 left-0 w-full flex justify-center items-center gap-x-4">
+        <Link text={"Acerca de Feedgames"} content={"About"} />
+        <Link text={"Aviso para los usuarios de iOS"} content={"Issues"} />
+      </footer>
     </div>
   );
 };
