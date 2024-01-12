ada78fc0bd61d12dd7b31822ef74c217e8b336a9
WatterAH <samueltlahuel.m@gmail.com>
2024-01-12 13:46:47 -0600

    feedgames v2.0 - images optimized

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -8,13 +8,14 @@ import { Link } from "react-router-dom";
 export const Post = ({ data }) => {
   const { id } = data;
   return (
-    <div className="w-full duration-700 p-6 shadow-sm border rounded-lg flex flex-col gap-y-4 hover:border-gray-300 hover:cursor-pointer">
-      <Link to={`/post/${id}`}>
-        <PostHeader data={data} />
-      </Link>
+    <Link
+      to={`/post/${id}`}
+      className="w-full duration-700 p-6 shadow-sm border rounded-lg flex flex-col gap-y-4 hover:border-gray-300 hover:cursor-pointer"
+    >
+      <PostHeader data={data} />
       <PostContent data={data}></PostContent>
       <TagsList data={data} />
       <Actions data={data} />
-    </div>
+    </Link>
   );
 };
--- frontend/src/components/Post/PostImage.jsx
+++ frontend/src/components/Post/PostImage.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export const PostImage = ({ publicUrl }) => {
-  return (
-    <Link to={publicUrl}>
-      <img src={publicUrl} alt="image" className="rounded-md" />
-    </Link>
-  );
+  return <img src={publicUrl} alt="image" className="rounded-md" />;
 };
