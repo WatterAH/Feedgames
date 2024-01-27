04c63d22648f39cae18821d6c977fafa1a180179
WatterAH <samueltlahuel.m@gmail.com>
2024-01-26 22:40:59 -0600

    tsc migration

--- frontend/src/components/Post/PostImage.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export var PostImage = function (_a) {
-    var publicUrl = _a.publicUrl, openModal = _a.openModal;
-    var src = "https://zptrwdrgobouoriwsfoj.supabase.co/storage/v1/object/public/Images/images/".concat(publicUrl);
-    return (React.createElement("img", { onClick: openModal, src: src, alt: "image", className: "rounded-md cursor-pointer" }));
-};
--- frontend/src/components/Post/TagsList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Tag } from "./Tag";
-export var TagsList = function (_a) {
-    var data = _a.data;
-    var tags = data.tags;
-    return (React.createElement("section", { className: "flex flex-wrap gap-2" }, tags.map(function (tag, index) { return (React.createElement(Tag, { text: tag, key: index })); })));
-};
