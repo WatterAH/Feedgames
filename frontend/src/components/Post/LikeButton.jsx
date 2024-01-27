f3e576524313382d4745cc2ef70df5f9b4a11118
WatterAH <samueltlahuel.m@gmail.com>
2024-01-26 22:43:45 -0600

    tsc migration

--- frontend/src/components/Post/LikeButton.jsx
+++ frontend/src/components/Post/LikeButton.jsx
@@ -4,6 +4,7 @@ import { faHeart as faHeartSolid } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons";
 import { toast } from "react-toastify";
 import { useUser } from "../../context/AuthContext";
+import { likePost, dontLikePost } from "../../Api/interactions";
 
 export const LikeButton = ({ likeData }) => {
   const { user } = useUser();
@@ -22,6 +23,7 @@ export const LikeButton = ({ likeData }) => {
         await dontLikePost(user.id, id, user.username, user_id);
       }
     } catch (error) {
+      console.log(error);
       const { message } = error;
       toast.error(message, {
         position: toast.POSITION.BOTTOM_CENTER,
