d0df253f15a047e870b462b6ccc5700114656f10
WatterAH <samueltlahuel.m@gmail.com>
2024-03-03 14:23:17 -0600

    follower management

--- frontend/src/components/Profile/ProfileFollowers.tsx
+++ frontend/src/components/Profile/ProfileFollowers.tsx
@@ -95,7 +95,7 @@ export const ProfileFollowers: React.FC<Props> = ({ userData }) => {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg flex justify-center font-medium leading-6 text-gray-900 p-2 border-b"
+                    className="text-lg flex justify-center font-medium leading-6 text-gray-900 p-2 border-b font-roboto"
                   >
                     Seguidores
                   </Dialog.Title>
