c0f8e816ad928b34f77ffc95012ed5bf8bebf947
WatterAH <samueltlahuel.m@gmail.com>
2024-03-13 15:21:10 -0600

    valorant posts update

--- frontend/src/components/CreateBlog/ValorantTracker.tsx
+++ frontend/src/components/CreateBlog/ValorantTracker.tsx
@@ -106,7 +106,7 @@ export const ValoranTracker: React.FC<Props> = ({
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg flex justify-center font-medium leading-6 text-gray-900 p-2 border-b font-roboto"
+                    className="text-xl flex justify-center font-medium leading-6 text-gray-900 p-2 border-b font-montserrat"
                   >
                     Tus partidas
                   </Dialog.Title>
--- frontend/src/components/Profile/ProfileFollows.tsx
+++ frontend/src/components/Profile/ProfileFollows.tsx
@@ -106,7 +106,7 @@ export const ProfileFollows: React.FC<Props> = ({ userData }) => {
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
                   <Dialog.Title
                     as="h3"
-                    className="text-lg flex justify-center font-medium leading-6 text-gray-900 border-b p-2 font-roboto"
+                    className="text-xl flex justify-center font-medium leading-6 text-gray-900 border-b p-2 font-montserrat"
                   >
                     Siguiendo
                   </Dialog.Title>
