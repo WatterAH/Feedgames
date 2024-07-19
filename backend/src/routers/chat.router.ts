fe261eced2ccf7760fa6eb7cbf555d1c56620b7d
WatterAH <samueltlahuel.m@gmail.com>
2024-07-19 08:58:43 -0600

    chat logic

--- backend/src/routers/chat.router.ts
+++ backend/src/routers/chat.router.ts
@@ -1,6 +1,3 @@
 import { Router } from "express";
-import { getFriendsById } from "../controllers/chat.controller";
 
 export const chatRouter = Router();
-
-chatRouter.get("/api/getFriendsById", getFriendsById);
