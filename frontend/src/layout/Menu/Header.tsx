9d9921ddccf7ba01b68a3b0703957cb08ce5b800
WatterAH <samueltlahuel.m@gmail.com>
2024-10-15 14:44:15 -0600

    redux first look

--- frontend/src/layout/Menu/Header.tsx
+++ frontend/src/layout/Menu/Header.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import Dropdown from "@/components/Global/Dropdown";
 import { useMenuOptions } from "@/hooks/useOptions";
 import { AlignRight } from "lucide-react";
+import { useUser } from "@/context/AuthContext";
 
 const Header = () => {
-  const options = useMenuOptions();
+  const { logout } = useUser();
+  const options = useMenuOptions(logout);
 
   return (
     <div className="fixed top-0 z-50 sm:h-10 bg-barcelona pb-4 pt-2 w-full lg:hidden backdrop-blur-md bg-opacity-80">
