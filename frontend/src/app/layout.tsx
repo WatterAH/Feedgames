2a133f7da997d33070f2b02152d86cb474d1f296
Samuel Tlahuel <107222607+WatterAH@users.noreply.github.com>
2025-01-17 12:04:44 -0600

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,7 +6,29 @@ import { Pacifico, Raleway, Montserrat, Inter } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "Feedgames",
-  description: "Share your passion for gaming.",
+  description: "Share your passion for gaming.",    
+  openGraph: {
+    title: "FeedGames: for gamers",
+    description: "FeedGames is the ultimate hub for passionate gamers. Share your best plays, analyze your match stats, and connect with a community that lives and breathes video games. Take your gaming experience to the next level!",
+    url: "https://feedgames.vercel.app",
+    siteName: "FeedGames",
+    images: [
+      {
+        url: "https://feedgames.vercel.app/icon.png", // Ruta a tu imagen representativa
+        width: 1200,
+        height: 630,
+        alt: "FeedGames - for gamers",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "FeedGames: for gamers",
+    description: "Comparte estadísticas de Valorant y más.",
+    images: ["https://feedgames.vercel.app/icon.png"],
+  },
 };
 
 const inter = Inter({
