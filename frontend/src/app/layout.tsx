ce607fbac972df2ab4d3194dc0232aff5c1ebb61
WatterAH <samueltlahuel.m@gmail.com>
2025-01-18 14:47:12 -0600

    Update metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,19 +7,18 @@ import { Pacifico, Raleway, Montserrat, Inter } from "next/font/google";
 export const metadata: Metadata = {
   title: "Feedgames",
   description: "Share your passion for gaming.",
-
   keywords:
     "gaming, community, feed, social, stats, valorant, esports, gamers, leaderboard, passion, share, follow, content creators, gameplay, interaction, statistics, gaming community, videojuegos, comunidad, feed, social, estadísticas, valorant, esports, gamers, clasificaciones, pasión, compartir, seguir, creadores de contenido, jugabilidad, interacción, estadísticas de juegos, comunidad gamer",
 
   openGraph: {
-    title: "Feedgames, para gamers",
+    title: "Feedgames, share your passion for gaming.",
     description:
       "FeedGames es el centro definitivo para jugadores apasionados. Comparte tus mejores jugadas, analiza las estadísticas de tus partidos y conéctate con una comunidad que vive y respira videojuegos. ¡Lleva tu experiencia de juego al siguiente nivel!",
     url: "https://feedgames.vercel.app",
     siteName: "Feedgames",
     images: [
       {
-        url: "https://feedgames.vercel.app/opengraph-image.png",
+        url: "https://feedgames.vercel.app/icon.png",
         width: 1200,
         height: 630,
         alt: "FeedGames - para gamers",
